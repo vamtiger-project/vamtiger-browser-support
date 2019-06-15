aadf4edde6e361ac28bcfafaa781ad039f053c60
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-15 14:22:23 +0200

    vamtiger-node-typescript-commit

--- build/vamtiger-aws-support.js
+++ build/vamtiger-aws-support.js
@@ -1,2 +1,2 @@
-!function(){"use strict";var s=window.VamtigerBrowserSupport,t={supportFile:"vamtiger-aws-support",supported:!!window.AWS};t.supported||(t.polyfill="https://unpkg.com/aws-sdk@latest/dist/aws-sdk.min.js"),s(t)}();
+!function(){"use strict";var s=window.VamtigerBrowserSupport,t={supportFile:"vamtiger-aws-support",supported:!!window.AWS};t.supported||(t.polyfill="https://cdn.jsdelivr.net/npm/aws-sdk@latest/dist/aws-sdk.min.js"),s(t)}();
 //# sourceMappingURL=vamtiger-aws-support.js.map
--- build/vamtiger-fetch-support.js
+++ build/vamtiger-fetch-support.js
@@ -1,2 +1,2 @@
-!function(){"use strict";var t=window.VamtigerBrowserSupport,p={supportFile:"vamtiger-fetch-support",supported:!!window.fetch};p.supported||(p.polyfill="https://unpkg.com/whatwg-fetch/fetch.js"),t(p)}();
+!function(){"use strict";var t=window.VamtigerBrowserSupport,e={supportFile:"vamtiger-fetch-support",supported:!!window.fetch};e.supported||(e.polyfill="https://cdn.jsdelivr.net/npm/whatwg-fetch@latest/fetch.js"),t(e)}();
 //# sourceMappingURL=vamtiger-fetch-support.js.map
